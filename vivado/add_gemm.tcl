0d89a4e4ffce42f042c33bef13a63f0820e7f7f3
RAMKRISHNA <ramkrishna012002@gmail.com>
2026-09-23 10:58:22 +0530

    Remove GEMM addition

--- vivado/add_gemm.tcl
+++
@@ -1,6 +0,0 @@
-# Add the 4x4 GEMM RTL and testbench to an existing Vivado project.
-set root [file normalize [file join [pwd] ..]]
-add_files [file join $root gemm gemm_4x4.v]
-add_files -fileset sim_1 [file join $root gemm gemm_4x4_tb.v]
-set_property top gemm_4x4_tb [get_filesets sim_1]
-puts "Added 4x4 INT8 GEMM RTL and testbench."
